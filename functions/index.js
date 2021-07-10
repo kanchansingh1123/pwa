44bb0a4d6faf4fc77b37c5c21834e2adb1cfb313
kanchan <kanchansingh1123@gmail.com>
2021-07-10 15:49:55 +0530

    Server side code to send push notification from server and listening them on UI side.

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,50 @@
-
 var functions = require('firebase-functions');
 var admin = require('firebase-admin');
 var cors = require('cors')({origin: true});
+var webpush = require('web-push');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
-var serviceAccount = require("./pwa-kanchan-firebase-adminsdk-4v6zp-77bcac58b8.json");
+var serviceAccount = require("./pwagram-fb-key.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://pwa-kanchan-default-rtdb.asia-southeast1.firebasedatabase.app/'
+  databaseURL: 'https://pwagram-99adf.firebaseio.com/'
 });
 
-exports.storePostData = functions.https.onRequest(function(request, response) {
- cors(request, response, function() {
-   admin.database().ref('posts').push({
-     id: request.body.id,
-     title: request.body.title,
-     location: request.body.location,
-     image: request.body.image
-   })
-     .then(function() {
-       response.status(201).json({message: 'Data stored', id: request.body.id});
-     })
-     .catch(function(err) {
-       response.status(500).json({error: err});
-     });
- });
+exports.storePostData = functions.https.onRequest(function (request, response) {
+  cors(request, response, function () {
+    admin.database().ref('posts').push({
+      id: request.body.id,
+      title: request.body.title,
+      location: request.body.location,
+      image: request.body.image
+    })
+      .then(function () {
+        webpush.setVapidDetails('mailto:kanchansingh1123@gmail.com', 'BO_JrB467fq8bqMgXiYhbWGEmnPSj0pJnDJMTGyAyOWuw9aQIF2fV_qrug4Z7cG0BTQhLl_yFXfKztltFRQDd-s', 'iP54fZO-7ELiiUjYnQSDugyFwD14PqRaI5d3uI9XTlc');
+        return admin.database().ref('subscriptions').once('value');
+      })
+      .then(function (subscriptions) {
+        subscriptions.forEach(function (sub) {
+          var pushConfig = {
+            endpoint: sub.val().endpoint,
+            keys: {
+              auth: sub.val().keys.auth,
+              p256dh: sub.val().keys.p256dh
+            }
+          };
+
+          webpush.sendNotification(pushConfig, JSON.stringify({title: 'New Post', content: 'New Post added!'}))
+            .catch(function(err) {
+              console.log(err);
+            })
+        });
+        response.status(201).json({message: 'Data stored', id: request.body.id});
+      })
+      .catch(function (err) {
+        response.status(500).json({error: err});
+      });
+  });
 });
